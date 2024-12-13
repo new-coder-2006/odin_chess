329e5ac9101936963181ac0b25b7d495b58c4fc7
Zach Lloyd <joseph.z.lloyd@gmail.com>
2024-12-12 22:17:52 -0500

    knight class created and passes all tests

--- knight.rb
+++ knight.rb
@@ -11,18 +11,70 @@ class Knight < Piece
     possible << [row + 2, col + 1] unless 
       (
         board[row + 2][col + 1].is_a?(Piece) && 
-        board[row + 2][col + 1].color == @ color) if (row + 2
+        board[row + 2][col + 1].color == @color
       ) || 
       (row + 2) > 8 || 
       (col + 1) > 8
-    possible << [row - 2, col - 1] if (row - 2) >= 0 && (col - 1) >= 0
-    possible << [row + 2, col - 1] if (row + 2) < 8 && (col - 1) >= 0
-    possible << [row - 2, col + 1] if (row - 2) >= 0 && (col + 1) < 8
-    possible << [row + 1, col + 2] if (row + 1) < 8 && (col + 2) < 8
-    possible << [row - 1, col - 2] if (row - 1) >= 0 && (col - 2) >= 0
-    possible << [row + 1, col - 2] if (row + 1) < 8 && (col - 2) >= 0
-    possible << [row - 1, col + 2] if (row - 1) >= 0 && (col + 2) < 8
+    possible << [row - 2, col - 1] unless 
+      (
+        board[row - 2][col - 1].is_a?(Piece) && 
+        board[row - 2][col - 1].color == @color
+      ) || 
+      (row - 2) < 0 || 
+      (col - 1) < 0
+    possible << [row + 2, col - 1] unless 
+      (
+        board[row + 2][col - 1].is_a?(Piece) && 
+        board[row + 2][col - 1].color == @color
+      ) || 
+      (row + 2) > 8 || 
+      (col - 1) < 0
+    possible << [row - 2, col + 1] unless 
+      (
+        board[row - 2][col + 1].is_a?(Piece) && 
+        board[row - 2][col + 1].color == @color
+      ) || 
+      (row - 2) < 0 || 
+      (col + 1) > 8
+    possible << [row + 1, col + 2] unless 
+      (
+        board[row + 1][col + 2].is_a?(Piece) && 
+        board[row + 1][col + 2].color == @color
+      ) || 
+      (row + 1) > 8 || 
+      (col + 2) > 8
+    possible << [row - 1, col - 2] unless 
+      (
+        board[row - 1][col - 2].is_a?(Piece) && 
+        board[row - 1][col - 2].color == @color
+      ) || 
+      (row - 1) < 0 || 
+      (col - 2) < 0
+    possible << [row + 1, col - 2] unless 
+      (
+        board[row + 1][col - 2].is_a?(Piece) && 
+        board[row + 1][col - 2].color == @color
+      ) || 
+      (row + 1) > 8 || 
+      (col - 2) < 0
+    possible << [row - 1, col + 2] unless 
+      (
+        board[row - 1][col + 2].is_a?(Piece) && 
+        board[row - 1][col + 2].color == @color
+      ) || 
+      (row - 1) < 0 || 
+      (col + 2) > 8
 
     possible
+  end
+
+  def move(new_row, new_col, board)
+    return false unless self.possible_moves(board).include?([new_row, new_col])
+    return false if check?(new_row, new_col, board)
+    
+    @row = new_row
+    @col = new_col
+
+    true
   end
 end
---
+++ spec/knight_spec.rb
@@ -0,0 +1,60 @@
+require_relative "../piece"
+require_relative "../knight"
+require_relative "../queen"
+require_relative "../king"
+require_relative "../bishop"
+
+
+describe Knight do
+  subject(:black_knight) { Knight.new(4, 4, "black") }
+  subject(:white_queen) { Queen.new(6, 5, "white") }
+  subject(:black_bishop) { Bishop.new(6, 3, "black") }
+  subject(:white_king) { King.new(3, 4, "white") }
+  subject(:black_king) { King.new(2, 4, "black")}
+
+  describe "#possible_moves" do
+    let(:test_board) { Array.new(8) { Array.new(8, nil) } }
+
+    context "when no other pieces on the board" do
+      before do
+        test_board[4][4] = black_knight
+      end
+
+      it "includes all spaces in the surrounding L-shapes" do
+        expect(black_knight.possible_moves(test_board)).to include(
+          [6, 5], [6, 3], [2, 5], [2, 3], [5, 6], [5, 2], [3, 6], [3, 2]
+        )
+      end
+
+      it "does not include space that the piece currently occupies" do
+        expect(black_knight.possible_moves(test_board)).not_to include([4, 4])
+      end
+
+      it "does not include spaces outside of the surrounding L-shapes" do
+        expect(black_knight.possible_moves(test_board)).not_to include([4, 5], [0, 0], [3, 3])
+      end
+    end
+
+    context "when other pieces are on the board" do
+      before do
+        test_board[4][4] = black_knight
+        test_board[6][5] = white_queen
+        test_board[6][3] = black_bishop
+        test_board[3][4] = white_king
+        test_board[2][4] = black_king
+      end
+
+      it "includes otherwise valid spaces that contain opponent's pieces" do
+        expect(black_knight.possible_moves(test_board)).to include([6, 5])
+      end
+
+      it "does not include otherwise valid spaces that contain player's pieces" do
+        expect(black_knight.possible_moves(test_board)).not_to include([6, 3])
+      end
+
+      it "is able to jump pieces" do
+        expect(black_knight.possible_moves(test_board)).to include([2, 3])
+      end
+    end
+  end
+end
