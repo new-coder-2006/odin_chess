18c9b26b00d955d536375570934a9b0fb4d36848
Zach Lloyd <joseph.z.lloyd@gmail.com>
2024-12-24 11:44:50 -0500

    Added coments to chess.rb

--- chess.rb
+++ chess.rb
@@ -21,11 +21,12 @@ class Chess
       "knight", 
       "rook"
     ]
-   
+    # Store pawns separately so en passant status can be cleared after each turn
     @black_pawns = []
     @white_pawns = []
-
+    # For each space in the first two rows
     (0..7).each do |i|
+      # Create the first row piece, then create the pawn on the second row
       @board[0][i] = create_piece(@first_row_pieces[i], 0, i, "black")
       @board[1][i] = create_piece("pawn", 1, i, "black")
       @black_pawns << @board[1][i]
@@ -33,7 +34,7 @@ class Chess
       @board[6][i] = create_piece("pawn", 6, i, "white")
       @white_pawns << @board[6][i]
     end
-
+    # Store kings seperately to facilitate checking for checkmate
     @black_king = @board[0][4]
     @white_king = @board[7][4]
   end
@@ -51,7 +52,7 @@ class Chess
     piece_hash[type].new(row, col, color)
   end
 
-
+  # Helper function for printing the appropriate symbol for each space
   def render_cell(row, col)
     space_contents = @board[row][col]
     if space_contents.nil?
@@ -83,6 +84,7 @@ class Chess
     false
   end
 
+  # Function to handle castling between a king and a rook
   def handle_castle(turn)
     puts "Please enter either left or right to identify the Rook you want to castle."
     side = gets.chomp
@@ -109,7 +111,7 @@ class Chess
         rook = @white_rook_right
       end
     end
-
+    # Check whether the specified pieces are actually eligible to castle
     return false unless king.can_castle?(side.downcase, @board) && rook.can_castle?(side.downcase, @board)
 
     if side.downcase == "left"
@@ -129,6 +131,8 @@ class Chess
 
   def get_row
     row = gets.chomp.downcase
+    # Return without doing anything else if the player specified they want to 
+    # castle
     return row if row == "castle"
 
     row_as_int = row.to_i
@@ -142,7 +146,7 @@ class Chess
   
   def get_col
     cols = ["a", "b", "c", "d", "e", "f", "g", "h"]
-    col = gets.chomp
+    col = gets.chomp.downcase
     unless cols.include?(col)
       puts "Please enter a valid column letter."
       return get_col
@@ -187,14 +191,18 @@ class Chess
         puts "Cannot castle on that side."
         return get_piece_to_move(turn)
       end
-
+      # If player chooses to castle and can legally castle, don't need to do
+      # anything else so just return "castled" as the from_row and nil for
+      # the piece to move and the from_col
       return [nil, "castled", nil]
     end
 
     from_col = get_from_col
     piece_to_move = @board[from_row][from_col]
-
+   
     if piece_to_move.nil? || piece_to_move.color != turn
+      # Handle case where player tries to select a space to move from and that 
+      # space doesn't include one of the  player's pieces
       puts "This space does not contain one of your pieces. Please enter the " +
            "coordinates for a space containing one of your pieces."
       print_board
@@ -206,7 +214,9 @@ class Chess
 
   def move_piece(piece)
     dest_row, dest_col = [get_to_row, get_to_col]
-
+    # If the player can legally make this move, piece.move moves the piece as 
+    # specified. Otherwise, print an error message and prompt the player for 
+    # another space to move to
     unless piece.move(dest_row, dest_col, @board)
       puts "You can't move to that space. Please enter a valid space to move " +
            "this piece to."
@@ -273,12 +283,14 @@ class Chess
     until winner?
       puts "It's #{turn}'s turn!"
       print_board
+      # Track whether or not the player has entered a valid move
       move_entered = false
+      # Set en passant to false for all the pieces of the player whose turn it is
       reset_en_passant(turn)
 
       until move_entered
         piece_to_move, from_row, from_col = get_piece_to_move(turn)
-        
+        # Move is handled via handle_castle if player is castling
         unless from_row == "castled"
           dest_row, dest_col = [get_to_row, get_to_col]
           turn_king = turn == "white" ? @white_king : @black_king
@@ -307,5 +319,5 @@ class Chess
   end
 end
 
-test_game = Chess.new
-test_game.game
+#test_game = Chess.new
+#test_game.game
